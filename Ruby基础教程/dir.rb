52e331c2395a6cf4cb46d9322ed5ebf2d7ea4a26
JohnTsai <johntsai.work@gmail.com>
2016-01-22 15:39:26 +0800

    add dir and file method

---
+++ Ruby基础教程/dir.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+require './fileRead'
+
+fileRead = FileRead.new(__FILE__)
+if ARGV.length == 0
+elsif ARGV[0] == 'true'|| ARGV[0]=='TRUE'||ARGV[0]=='True'
+	fileRead.filtprint
+elsif ARGV[0] == 'false'|| ARGV[0]=='FALSE'||ARGV[0]=='False'
+	fileRead.nofiltprint
+else
+end
+# http://www.runoob.com/ruby/ruby-dir-methods.html
+
+$URL = "http://www.runoob.com/ruby/ruby-dir-methods.html"
+puts "More methods,visit #{$URL}"
