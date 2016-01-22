a1f3a8f7b92da88d8ceab92cc5725c46e9879312
JohnTsai <johntsai.work@gmail.com>
2016-01-22 23:14:02 +0800

    rename dir

---
+++ RubyBasic/exceptions.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+require './fileRead'
+
+fileRead = FileRead.new(__FILE__)
+if ARGV.length == 0
+elsif ARGV[0] == 'true'|| ARGV[0]=='TRUE'||ARGV[0]=='True'
+	fileRead.filtprint
+elsif ARGV[0] == 'false'|| ARGV[0]=='FALSE'||ARGV[0]=='False'
+	fileRead.nofiltprint
+else
+end
+#Ruby 异常
+# begin #开始
+ 
+#  raise.. #抛出异常
+ 
+# rescue [ExceptionType = StandardException] #捕获指定类型的异常 缺省值是StandardException
+#  $! #表示异常信息
+#  $@ #表示异常出现的代码位置
+# else #其余异常
+#  ..
+# ensure #不管有没有异常，进入该代码块
+ 
+# end #结束
+
+begin
+	file = open("./unexistant_file")
+	if file
+		puts "open success"
+	end
+	rescue
+		puts STDIN
+	end
---
+++ trans_to_url.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+$URL_PREFIX = "https://github.com/JohnTsaiAndroid/LearnRuby/tree/master/RubyBasic/"
+
+if File::directory?("./RubyBasic")
+  dir = Dir::entries("./RubyBasic")
+  file = File.new("./url.txt","w+")
+  puts dir
+  if file
+  	str = ""
+  	dir.each do |f|
+  		if(f=~/^[.]/)
+  		else
+  	       str+=("[#{f}](#{$URL_PREFIX}#{f})\n")
+  	   end
+    end
+    puts str
+    file.syswrite(str)
+    file.close
+   end
+end
