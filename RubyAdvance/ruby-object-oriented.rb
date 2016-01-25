a3df2185e6ca6e8b2ab9e4d4397acdd8a3001c33
JohnTsai <johntsai.work@gmail.com>
2016-01-25 18:21:33 +0800

    update ruby-oo

--- RubyAdvance/ruby-object-oriented.rb
+++ RubyAdvance/ruby-object-oriented.rb
@@ -227,4 +227,56 @@ box4.printInfo
 #http://rubylearning.com/satishtalim/ruby_self.html
 puts self
 
+# http://stackoverflow.com/questions/6669527/use-of-ruby-self-keyword
+
+class Fooo
+	def self.test
+		puts "#{self.to_s} class method #{self.class.name}"
+	end
+	def test
+		puts "#{self.to_s} instance method #{self.class.name}"
+	end
+end
+
+Fooo.test
+
+fooo = Fooo.new
+fooo.test
+
+class Bar
+  def self.foo
+    "foo!"
+  end
+
+  def baz
+    "baz!"
+  end
+
+  def self.success
+    foo #looks for variable foo, doesn't find one, looks for class method foo, finds it, returns "foo!"
+  end
+
+  def self.fail
+    baz #looks for variable baz, doesn't find one, looks for class method baz, doesn't find one, raises exception
+  end
+
+  def instance_success
+    baz #looks for variable baz, doesn't find one, looks for instance method baz, finds it, returns "baz!"
+  end
+
+  def instance_fail
+    foo #looks for variable foo, doesn't find one, looks for instance method foo, doesn't find one, raises exception
+  end
+
+  def local_variable
+    baz = "is my favorite method"
+    baz #looks for variable baz, finds it, returns "is my favorite method"
+  end
+
+  def disambiguate
+    baz = " is my favorite method"
+    self.baz + baz #looks for instance method baz, finds it, looks for local variable baz, finds it, returns "baz! is my favorite method"
+  end
+end
+
 
